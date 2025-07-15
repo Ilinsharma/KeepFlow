58ad0eced8c89e2e332b3a06add438b142f46446
Ilin Sharma <ilinsharma376@gmail.com>
2025-07-16 03:33:05 +0530

    Disabled ESLint blocking during Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // ✅ This line disables lint errors during build
+    ignoreDuringBuilds: true,
+  },
+  // any other config...
 };
 
 export default nextConfig;
